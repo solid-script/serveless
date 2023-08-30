18dc034fc9ef25e01305e11f5da051769f4fe60e
HenryRuiz332 <henryruiz332@gmail.com>
2023-08-30 14:02:39 -0400

    export default AutoType;

--- javascript/AutoType.js
+++ javascript/AutoType.js
@@ -1,54 +1,41 @@
-// Contenido de AutoType.js (versión adaptada para script estándar)
+class AutoType {
+  constructor(techArray, targetElementId, speed) {
+    this.i = 0;
+    this.speed = speed || 50;
+    this.tech = techArray || [];
+    this.currentIndex = 0;
+    this.targetElementId = targetElementId || "demo";
+    this.startTyping();
+  }
 
-function AutoType(techArray, targetElementId, speed) {
-  this.i = 0;
-  this.speed = speed || 50;
-  this.tech = techArray || [];
-  this.currentIndex = 0;
-  this.targetElementId = targetElementId || "demo";
-  this.startTyping();
-}
-
-AutoType.prototype.autoType = function () {
-  if (this.currentIndex < this.tech.length) {
-    if (this.i < this.tech[this.currentIndex].length) {
-      document.getElementById(this.targetElementId).innerHTML += this.tech[this.currentIndex].charAt(this.i);
-      this.i++;
-      const _this = this;
-      setTimeout(function () {
-        _this.autoType();
-      }, this.speed);
-    } else {
-      const _this = this;
-      setTimeout(function () {
-        _this.eraseText();
-      }, this.speed * 3);
+  autoType() {
+    if (this.currentIndex < this.tech.length) {
+      if (this.i < this.tech[this.currentIndex].length) {
+        document.getElementById(this.targetElementId).innerHTML += this.tech[this.currentIndex].charAt(this.i);
+        this.i++;
+        setTimeout(() => this.autoType(), this.speed);
+      } else {
+        setTimeout(() => this.eraseText(), this.speed * 3);
+      }
     }
   }
-};
 
-AutoType.prototype.eraseText = function () {
-  if (this.i >= 0) {
-    let currentText = this.tech[this.currentIndex].substring(0, this.i);
-    document.getElementById(this.targetElementId).innerHTML = currentText;
-    this.i--;
-    const _this = this;
-    setTimeout(function () {
-      _this.eraseText();
-    }, this.speed);
-  } else {
-    this.currentIndex = (this.currentIndex + 1) % this.tech.length;
-    this.i = 0;
-    const _this = this;
-    setTimeout(function () {
-      _this.autoType();
-    }, this.speed);
+  eraseText() {
+    if (this.i >= 0) {
+      let currentText = this.tech[this.currentIndex].substring(0, this.i);
+      document.getElementById(this.targetElementId).innerHTML = currentText;
+      this.i--;
+      setTimeout(() => this.eraseText(), this.speed);
+    } else {
+      this.currentIndex = (this.currentIndex + 1) % this.tech.length;
+      this.i = 0;
+      setTimeout(() => this.autoType(), this.speed);
+    }
   }
-};
 
-AutoType.prototype.startTyping = function () {
-  this.autoType();
-};
+  startTyping() {
+    this.autoType();
+  }
+}
+export default AutoType;
 
-// Exportar la clase AutoType para uso global (opcional)
-window.AutoType = AutoType;
